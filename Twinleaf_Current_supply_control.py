fffe3d4c26821afb2dbcc5e25c2e694749bb9d27
Yinan <1181504787@qq.com>
2018-07-25 20:31:06 +0200

    Update Twinleaf_Current_supply_control.py

--- Twinleaf_Current_supply_control.py
+++ Twinleaf_Current_supply_control.py
@@ -10,35 +10,35 @@ csb = tldevice.Device('COM5')
 
 root = Tk()
 root.geometry("600x600+300+0")
-root.title('Current Supply Control Twinleaf')  # 设置窗口标题
+root.title('Current Supply Control Twinleaf')  # title
 v = StringVar()
 s1 = Scale(root,from_ = -40,
-      to = 40,#设置最大值
-      orient = HORIZONTAL,#设置横向
+      to = 40,#max value
+      orient = HORIZONTAL,# direction
       label='X channel:',
-      resolution=0.001,#设置步长
-      tickinterval = 10,#设置刻度
-      length = 600)# 设置像素
+      resolution=0.001,#step
+      tickinterval = 10,
+      length = 600)
 s1.pack()
 s2 = Scale(root,
-      from_ = -40,#设置最小值
-      to = 40,#设置最大值
-      orient = HORIZONTAL,#设置横向
+      from_ = -40,#min value
+      to = 40,
+      orient = HORIZONTAL,
       label='Y channel:',
-      resolution=0.001,#设置步长
-      tickinterval = 10,#设置刻度
-      length = 600)# 设置像素
+      resolution=0.001,
+      tickinterval = 10,
+      length = 600)
 
 s2.pack()
 s3 = Scale(root,
-      from_ = -40,#设置最小值
-      to = 40,#设置最大值
-      orient = HORIZONTAL,#设置横向
+      from_ = -40,#
+      to = 40,#
+      orient = HORIZONTAL,#
       label='Z channel:',
-      resolution=0.001,#设置步长
-      tickinterval = 10,#设置刻度
-      length = 600,# 设置像素
-      variable = v)#绑定变量
+      resolution=0.001,#
+      tickinterval = 10,#
+      length = 600,# 
+      variable = v)#
 s3.pack()
 print(v.get())
 
@@ -59,29 +59,29 @@ def zshow():
     print(s1.get(),s2.get(),s3.get())
     print(csb.coil.x.current(),csb.coil.y.current(),csb.coil.z.current())
     csb.coil.z.current(s3.get())  # mA
-Output_btn=Button(root,text = 'X_Current_output',command = xshow,anchor='w') #用command回调函数获取位置
+Output_btn=Button(root,text = 'X_Current_output',command = xshow,anchor='w') #
 Output_btn.pack()
-Output_btn=Button(root,text = 'Y_Current_output',command = yshow,anchor='w') #用command回调函数获取位置
+Output_btn=Button(root,text = 'Y_Current_output',command = yshow,anchor='w') #
 Output_btn.pack()
-Output_btn=Button(root,text = 'Z_Current_output',command = zshow,anchor='w') #用command回调函数获取位置
+Output_btn=Button(root,text = 'Z_Current_output',command = zshow,anchor='w') 
 Output_btn.pack()
 
 l1 = Label(root, text="X input(mA)：",anchor='w')
-l1.pack(side=TOP, anchor=W, fill=X, expand=YES)  # 这里的side可以赋值为LEFT  RTGHT TOP  BOTTOM
+l1.pack(side=TOP, anchor=W, fill=X, expand=YES)  # 
 xls_text = StringVar()
 xls = Entry(root, textvariable=xls_text)
 xls_text.set(" ")
 xls.pack(side=TOP, anchor=W, fill=X, expand=NO)
 
 l2 = Label(root, text="Y input(mA)：",anchor='w')
-l2.pack(side=TOP, anchor=W, fill=X, expand=YES)  # 这里的side可以赋值为LEFT  RTGHT TOP  BOTTOM
+l2.pack(side=TOP, anchor=W, fill=X, expand=YES)  # 
 sheet_text = StringVar()
 sheet = Entry(root, textvariable=sheet_text)
 sheet_text.set(" ")
 sheet.pack(side=TOP, anchor=W, fill=X, expand=NO)
 
 l3 = Label(root, text="Z input(mA)：",anchor='w')
-l3.pack(side=TOP, anchor=W, fill=X, expand=YES)  # 这里的side可以赋值为LEFT  RTGHT TOP  BOTTOM
+l3.pack(side=TOP, anchor=W, fill=X, expand=YES)  
 loop_text = StringVar()
 loop = Entry(root, textvariable=loop_text)
 loop_text.set(" ")
